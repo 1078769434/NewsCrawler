2f8bfbd1ac0d31a16b6f98c33619499fabe1b12b
limao <1078769434@qq.com>
2025-01-24 09:58:10 +0800

    feat(news): 添加网易新闻爬虫功能
    - 新增 NeteaseNewsSpider 类，实现网易新闻爬取功能
    - 添加 RequestHandler 类处理 HTTP 请求
    - 实现 TencentNewsDataParser 类解析新闻数据- 更新项目依赖，增加 beautifulsoup4 和 bs4

---
+++ news/netease/netease_crawl.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# Date:2025-01-23 16 57
+import asyncio
+
+from argon_log import logger
+from news.netease.request_handler import RequestHandler
+from news.netease.data_parser import TencentNewsDataParser
+from parse.news_parse import get_news_content
+from save.database_handler import DatabaseHandler
+
+
+class NeteaseNewsSpider:
+    def __init__(self):
+        self.request_handler = RequestHandler()
+        self.data_parser = TencentNewsDataParser()
+        self.database_handler = DatabaseHandler()
+        self.latest_china_news_url = "https://news.163.com/special/cm_guonei/"
+        self.hot_news_url = "https://news.163.com/domestic/"
+
+    async def fetch_hot_news(self):
+        logger.info("开始抓取网易新闻-热门新闻...")
+        response_text = await self.request_handler.fetch_data(self.hot_news_url)
+        if response_text:
+            # 解析数据
+            json_data = self.data_parser.parse_hot_news_data(response_text)
+            if json_data:
+                # 并发请求新闻页面的 HTML
+                tasks = [self.process_news(news) for news in json_data]
+                await asyncio.gather(*tasks)
+
+    async def fetch_latest_china_news(self):
+        logger.info("开始抓取网易新闻-最新国内新闻...")
+        response_text = await self.request_handler.fetch_data(
+            self.latest_china_news_url
+        )
+        if response_text:
+            # 解析数据
+            json_data = self.data_parser.extract_json_from_callback(response_text)
+            if json_data:
+                # 并发请求新闻页面的 HTML
+                tasks = [self.process_news(news) for news in json_data]
+                await asyncio.gather(*tasks)
+
+    async def process_news(self, news):
+        """
+        处理单条新闻：获取 HTML 并解析新闻内容。
+
+        :param news: 单条新闻数据
+        """
+        # 获取新闻页面的 HTML
+        news_html = await self.request_handler.fetch_data(news["docurl"])
+        if news_html:
+            news_content = get_news_content(news_html)
+            news_content["url"] = news["docurl"]
+            logger.info(f"网易新闻 HTML 解析成功:{news_content}")
+            return news_content
+        else:
+            logger.error(f"获取网易新闻 HTML 失败: {news['url']}")
+
+
+if __name__ == "__main__":
+    spider = NeteaseNewsSpider()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(spider.fetch_hot_news())
