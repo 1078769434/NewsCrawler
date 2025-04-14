32bdd21d0c9281569d62ad46f3f247c0753fbb60
limao <1078769434@qq.com>
2025-04-14 13:49:02 +0800

    feat(db): 优化数据库初始化流程并添加数据存储功能
    -优化数据库初始化流程，仅在数据库为空时创建表
    - 新增 NewsStorage 类，用于将新闻数据保存为 JSON 或 CSV 文件
    - 支持按新闻源名称和时间戳生成文件名

--- db/init_db.py
+++ db/init_db.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from loguru import logger
+from sqlalchemy import inspect
 
 # ⚠️ warning  import register model here, your class must  import in models.__init__.py file
 import model  # noqa: F401
@@ -16,5 +17,20 @@ async def async_main():
         await conn.run_sync(Base.metadata.create_all)
 
 
+async def async_init_db():
+    async with async_engine.begin() as conn:
+        # 使用 run_sync 执行同步操作
+        inspector = await conn.run_sync(inspect)
+        existing_tables = inspector.get_table_names()
+
+        # 如果没有找到任何表，表示数据库为空，才进行表创建
+        if not existing_tables:
+            logger.info("未找到表，正在创建表...")
+            await conn.run_sync(Base.metadata.create_all)
+            logger.info("表创建成功。")
+        else:
+            logger.info("表已存在，跳过创建。")
+
+
 if __name__ == "__main__":
-    asyncio.run(async_main())
+    asyncio.run(async_init_db())
