d219376c7fe5393a3256727644571bc0a93fbfbd
limao <1078769434@qq.com>
2025-04-14 14:01:05 +0800

    refactor(base_spider): 重构 fetch_and_process_news 方法

--- base/base_spider.py
+++ base/base_spider.py
@@ -16,7 +16,23 @@ class BaseSpider(abc.ABC):
 
     # 获取新闻并处理
     @abc.abstractmethod
-    async def fetch_and_process_news(self):
+    async def fetch_and_process_news(
+        self,
+        url: str,
+        category: str,
+        source: str,
+        log_prefix: str,
+        parse_method: callable,
+    ):
+        """
+        抓取并处理新闻数据。
+
+        :param url: 新闻数据的 URL
+        :param category: 新闻分类
+        :param source: 新闻来源
+        :param log_prefix: 日志前缀
+        :param parse_method: 解析数据的方法
+        """
         pass
 
     # 处理单条新闻
